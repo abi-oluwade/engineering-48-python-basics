8a757b0a4057654ee459b8e6fc4258a27b1c09c3
Abi Oluwade <abi.oluwade1@gmail.com>
2019-12-02 23:17:19 +0000

    third set of python excercises partially completed

---
+++ python_exercises_day3.py
@@ -0,0 +1,29 @@
+
+#1 - Define a dictionary call story1, it should have the followign keys:
+        # hero_name, hero_age, start, middle and end
+d = {'hero_name': "Tommy Wiseau", 'hero_age': 40, 'Start': "It was a cold winter morning", 'Middle': "I was making my way downtown",
+     'End': "walking fast, faces pass and I'm home bound"}
+
+#2 - Print the entire dictionary
+print(d)
+
+#3 - Print the type of your dictionary
+print((type(d)))
+
+#4 - Print only the keys
+print('hero_name')
+print('hero_age')
+
+#4 - print only the values
+print(d.get('hero_name'))
+print(d.get('hero_age'))
+print(d.get('Start'))
+print(d.get('Middle'))
+print(d.get('End'))
+
+#5 - print the individual values using the keys (individually, lots of printi commands)
+
+#6 - Now let's reassing the exist key value pairs with user inputs
+    # - hero_name, hero_age, hero_power
+
+#7 print the story in an organised/formated manor
